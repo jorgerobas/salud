9d16dac5027b698bcc9cdbd9d57b74b7f23e5079
JJ <javier_robas@hotmail.com>
2018-01-13 19:40:59 -0300

    volvimos a empezar

---
+++ Logica/gestorUsuarios.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using Entidades;
+
+namespace Logica
+{
+    public class gestorUsuarios
+    {
+        public List<Usuario> ListarUsuarios()
+        {
+            try
+            {
+                using (consultoriosEntities dbContext = new consultoriosEntities())
+                {
+                    var Query = (from n in dbContext.usuarios
+                                 select new Usuario
+                                 {
+                                     Id = n.Id,
+                                     nombre=n.nombre,
+                                     password=n.password,
+                                     tipo_usuario=n.tipo_usuario
+                                     
+                                 }).ToList();
+                    return Query;
+                }
+            }
+            catch (Exception)
+            {
+                throw;  
+            }
+        }
+    }
+}
