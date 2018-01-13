c3fac854bf5ac49b5eca91588eaa13ba93075d7b
matiasff <matybienalto@gmail.com>
2018-01-13 20:57:23 -0300

    se empieza a trabajar

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+-- INSERTS DE LA TABLA USUARIOS
+
+INSERT INTO [dbo].[usuarios]   ([nombre],[password],[tipo_usuario]) VALUES('jrobas','jrobas','admin');
+INSERT INTO [dbo].[usuarios]   ([nombre],[password],[tipo_usuario]) VALUES('mfabbro','mfabbro','admin');
+INSERT INTO [dbo].[usuarios]   ([nombre],[password],[tipo_usuario]) VALUES('jperez','jperez','user');
+
+select * from [dbo].[usuarios];
+
